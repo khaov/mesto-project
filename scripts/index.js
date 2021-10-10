9cb895989b85477189f03bd8b4671419ae443f9e
Artyom Khaov <a.khaov@gmail.com>
2021-10-11 00:06:04 +0700

    Added: card delete functional

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,17 @@ const cardTemplate = document.querySelector('.card-template').content;
 
 function addCard(nameValue, linkValue) {
   const cardsItem = cardTemplate.cloneNode(true);
+  const deleteButton = cardsItem.querySelector('.card__delete-button');
+
   cardsItem.querySelector('.card__name').textContent = nameValue;
   cardsItem.querySelector('.card__image').src = linkValue;
   cardsItem.querySelector('.card__image').alt = nameValue;
   cardsList.prepend(cardsItem);
+
+  deleteButton.addEventListener('click', function () {
+    const currentItem = deleteButton.closest('.cards__item');
+    currentItem.remove();
+  });
 }
 
 initialCards.forEach(function (card) {
